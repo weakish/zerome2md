bb210bf817829d36a7f13f3c6a5278afb0a0cbfa
Jakukyo Friel <weakish@gmail.com>
2018-12-05 21:55:01 +0800

    add support for images

--- main.go
+++ main.go
@@ -4,7 +4,11 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/weakish/goaround"
+	"github.com/weakish/gosugar"
 	"io/ioutil"
+	"path/filepath"
+	"strconv"
+	"strings"
 	"time"
 )
 
@@ -24,8 +28,23 @@ func main() {
 	var posts []post
 	goaround.PanicIf(json.Unmarshal(user["post"], &posts))
 
+	var images []string
+	images, err = filepath.Glob("*.jpg")
+	goaround.PanicIf(err)
+
+	var imageIds gosugar.StringSet = gosugar.NewStringSet()
+	for _, image := range images {
+		imageIds.Add(strings.TrimSuffix(image, ".jpg"))
+	}
+
 	for _, p := range posts {
 		fmt.Println(p.Body + "\n")
+
+		var id string = strconv.Itoa(p.PostId)
+		if imageIds.Contains(id) {
+			fmt.Println("![attached image](" + id + ".jpg)\n")
+		}
+
 		var date time.Time = time.Unix(p.DateAdded, 0).UTC()
 		fmt.Println("-- " + date.Format("2006-01-02") + "\n\n")
 	}
