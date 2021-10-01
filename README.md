6860a39a90a6a96097322e436eea19e82ed0a7cd
Jang Rush <weakish@gmail.com>
2021-10-02 02:35:47 +0800

    :new: export to git commit messages

--- README.md
+++ README.md
@@ -30,6 +30,26 @@ http://127.0.0.1:43110/Me.ZeroNetwork.bit/?Profile/HUBID/USERID/yourid@zeroid.bi
 Then you should run `zerome2md` under
 `ZeroNet/data/HUBID/data/users/USERID`
 
+### Convert to git commit messages
+
+I have seen a lot of people use GitHub Issues as a blog system.
+But I have not seen any one use git commit messages as a blog system.
+
+zerome2md can convert ZeroMe posts to git commit messages.
+Note that this is a one time export and an empty git repository is preferred.
+
+```sh
+mkdir /path/to/example
+cd /path/to/example
+git init
+
+cd ZeroNet/data/HUBID/data/users/USERID
+zerome2md -p=https://example.com/path -r=/path/to/example -n=YOUR_NAME -e=YOUR_EMAIL
+```
+
+`https://example.com/path` is where you put all of your zerome `*.jpg` files.
+`YOUR_NAME` and `YOUR_EMAIL` will be used in the git commit message.
+
 ## License
 
 0BSD
--- main.go
+++ main.go
@@ -2,18 +2,22 @@ package main
 
 import (
 	"encoding/json"
+	"flag"
 	"fmt"
-	"github.com/weakish/gosugar"
 	"io/ioutil"
 	"path/filepath"
 	"strconv"
 	"strings"
 	"time"
+
+	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing/object"
+	"github.com/weakish/gosugar"
 )
 
 type post struct {
-	PostId    int `json:"post_id"`
-	DateAdded int64 `json:"date_added"`
+	PostId    int    `json:"post_id"`
+	DateAdded int64  `json:"date_added"`
 	Body      string `json:"body"`
 	Meta      string `json:"meta"`
 }
@@ -34,15 +38,35 @@ func main() {
 		imageIds.Add(strings.TrimSuffix(image, ".jpg"))
 	}
 
+	prefix := flag.String("p", "", "site prefix")
+	repo := flag.String("r", ".", "repository directory")
+	name := flag.String("n", "zerome", "git author name")
+	email := flag.String("e", "", "git author email")
+	flag.Parse()
 	for _, p := range posts {
-		fmt.Println(p.Body + "\n")
-
 		var id string = strconv.Itoa(p.PostId)
-		if imageIds.Contains(id) {
-			fmt.Println("![attached image](" + id + ".jpg)\n")
-		}
-
 		var date time.Time = time.Unix(p.DateAdded, 0).UTC()
-		fmt.Println("-- " + date.Format("2006-01-02") + "\n\n")
+		if *prefix == "" {
+			fmt.Println(p.Body + "\n")
+			if imageIds.Contains(id) {
+				fmt.Println("![attached image](" + id + ".jpg)\n")
+			}
+			fmt.Println("-- " + date.Format("2006-01-02") + "\n\n")
+		} else {
+			body := p.Body + "\n"
+			if imageIds.Contains(id) {
+				body += "![attached image](" + *prefix + "/" + id + ".jpg)\n"
+			}
+
+			r, _ := git.PlainOpen(*repo)
+			w, _ := r.Worktree()
+			w.Commit(body, &git.CommitOptions{
+				Author: &object.Signature{
+					Name:  *name,
+					Email: *email,
+					When:  date,
+				},
+			})
+		}
 	}
 }
